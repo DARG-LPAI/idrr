0ae23e13bbacd743b8aa6889002e2b714c74d7d1
vssun16 <vssun_1006@163.com>
2025-11-27 14:53:11 +0800

    feat: update compute_score function to implement long-tail reward calculation

--- verl/utils/reward_score/pdtb.py
+++ verl/utils/reward_score/pdtb.py
@@ -25,18 +25,24 @@ def compute_score(solution_str: str, ground_truth: str, algorithm: str = 'grpo')
     answer_text, processed_str = extract_solution(solution_str)
     # 成功解析
     if answer_text and processed_str:
-        # print("\n[正确性验证]")
-        # print(f"  真实标签: {ground_truth}")
-        # print(f"  预测标签: {answer_text}")
-        # print(f"\n[模型思考过程为]\n{processed_str}")
-        # 检验答案是否正确
-        
-        if answer_text == ground_truth:
+        if algorithm == 'grpo': # 长尾奖励
+            alpha = 0.6 # 控制对长尾分布的关注程度
+            label2weight = {
+                'A': 1.32,
+                'B': 1.13,
+                'C': 1.0,
+                'D': 2.0,
+            }
+            if answer_text == ground_truth:
+                total_score = (1 - alpha) * 1.0 + alpha * label2weight.get(ground_truth, 1.0)
+            else:
+                total_score = -0.1
+        elif answer_text == ground_truth: # 标准二值奖励
             total_score = 1
         else:
-            total_score = -1
+            total_score = -1 # 错误答案得分
     else:
-        total_score = -1
+        total_score = -1 # 解析失败得分
     # print(f" 最终得分{total_score} ".center(80, '-'))
 
     if algorithm == 'dapo':
@@ -52,5 +58,5 @@ def compute_score(solution_str: str, ground_truth: str, algorithm: str = 'grpo')
 if __name__ == "__main__":
     # 测试代码
     answer_text = r'\boxed{A}'
-    answer = remove_boxed(answer_text)
-    print(f"去除boxed后的答案为: {answer}")
+    # answer = remove_boxed(answer_text)
+    # print(f"去除boxed后的答案为: {answer}")
