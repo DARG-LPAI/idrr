58d181f088fa8cc7561fb17b2bf475740a714380
vssun16 <vssun_1006@163.com>
2025-12-29 21:35:36 +0800

    feat: update vllm_offline_infer.sh for supporting evaluation

--- scripts/vllm_offline_infer.sh
+++ scripts/vllm_offline_infer.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=7
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 # python src/offline_infer.py \
@@ -11,12 +11,17 @@ export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 #     # --ckpt /data/whsun/idrr/expt/arg2def/pdtb2/llama3/epo5/merged \
 
 # 如果是 dataset_infos 里面定义的 dataset，可直接用如下脚本运行
+save_name=results/rl_cold_start/pdtb2/qwen3-1.7b-exp_by_qwen3/epo1/generated_predictions.jsonl
 python /data/whsun/LLaMA-Factory/scripts/vllm_infer.py \
-    --dataset pdtb2_test_rl_cold_start \
-    --save_name results/rl_cold_start/pdtb2/Qwen3-8B/merged.vllm.pred.test.json \
-    --model_name_or_path expt/rl_cold_start/pdtb2/Qwen3-8B/epo1/lora_merged/lora_merged \
+    --dataset pdtb2_test_qwen3_max_exp \
+    --model_name_or_path expt/rl_cold_start/pdtb2/qwen3-1.7b-exp_by_qwen3/epo1/lora_merged \
+    --adapter_name_or_path None \
     --vllm_config {"gpu_memory_utilization":0.9} \
     --template qwen3 \
     --cutoff_len 1024 \
     --max_new_tokens 2048 \
     --top_k 20 \
+    --save_name $save_name \
+
+# 评测脚本
+python src/eval.py --data_path $save_name
