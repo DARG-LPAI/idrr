ed5b90c8027f5756e38d488f5ca31f2557490533
vssun16 <vssun_1006@163.com>
2026-01-05 17:01:43 +0800

    fix: update base_model for tokenizer_base

--- scripts/merge_lora.sh
+++ scripts/merge_lora.sh
@@ -1,7 +1,8 @@
-export CUDA_VISIBLE_DEVICES=2
-lora_path=expt/rl_cold_start/pdtb2/Qwen2.5-7B-Instruct/epo1
+export CUDA_VISIBLE_DEVICES=1
+base_model=/data/whsun/idrr/expt/rl_cold_start/pdtb2/top/qwen3-8b-exp_by_qwen3/epo1/lora_merged
+lora_path=/data/whsun/idrr/checkpoints/verl_pdtb/Qwen3-8B-E1_by_qwen3_max-DAPO-lora/global_step_210/actor/lora_adapter
 python src/lora_merge.py \
-    --base_model ../pretrained_models/Qwen/Qwen2.5-7B-Instruct \
+    --base_model $base_model \
+    --tokenizer_base $base_model \
     --lora_path $lora_path \
-    --tokenizer_base $lora_path \
     --output_dir $lora_path/lora_merged
